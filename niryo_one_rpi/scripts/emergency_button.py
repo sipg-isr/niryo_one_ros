890f1724fd61d4bc6ecc89c389e3586b59648fdf
candeiasalexandre <candeiasalexandre@gmail.com>
2019-05-15 11:36:16 +0100

    changed the way emergency button is launched - now I'm using the rpi manager - needs testing

--- niryo_one_rpi/scripts/emergency_button.py
+++ niryo_one_rpi/scripts/emergency_button.py
@@ -5,7 +5,7 @@ import time
 from niryo_one_python_api.niryo_one_api import *
 from std_msgs.msg import Bool
 
-rospy.init_node("emergency_button", anonymous=True)
+rospy.init_node("niryo_emergency_button", anonymous=True)
 tmp = rospy.wait_for_message("/niryo_one/learning_mode", Bool)
 
 n = NiryoOne()
@@ -14,17 +14,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
              
 def callback_function_falling(channel):
-    print("Button Activated...")
+    #print("Button Activated...")
     try:
         n.activate_learning_mode(True)
-        print("Robot Learning Mode Active")
+       # print("Robot Learning Mode Active")
     except NiryoOneException as e:
-        print(e)
+        rospy.logerr("Niryo_One/Emergency_button: Error Calback Function")
+        #print(e)
 
 GPIO.add_event_detect(16, GPIO.FALLING, callback=callback_function_falling)
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+r = rospy.Rate(1)
+while not rospy.is_shutdown():
+        r.sleep()
 GPIO.cleanup()
